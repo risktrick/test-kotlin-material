ce92f6dbdcd40ebc8a6329c7460d94b0c1dfc6b8
RiskTrick <risktrick@gmail.com>
2017-11-10 18:26:56 +0100

    toogle staggered layout from list to grid

--- app/src/main/java/com/raywenderlich/android/travelwishlist/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/travelwishlist/MainActivity.kt
@@ -66,6 +66,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showListView() {
+        staggeredLayoutManager.spanCount = 1
+
         val item = menu.findItem(R.id.action_toggle)
         item.setIcon(R.drawable.ic_action_grid)
         item.title = getString(R.string.show_as_grid)
@@ -73,6 +75,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showGridView() {
+        staggeredLayoutManager.spanCount = 2
+
         val item = menu.findItem(R.id.action_toggle)
         item.setIcon(R.drawable.ic_action_list)
         item.title = getString(R.string.show_as_list)
