1544e7af67433390a04385d071b5754e57d3b221
RiskTrick <risktrick@gmail.com>
2017-11-10 17:29:15 +0100

    initial

---
+++ app/src/main/java/com/raywenderlich/android/travelwishlist/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.raywenderlich.android.travelwishlist
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.StaggeredGridLayoutManager
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit private var menu: Menu
+    private var isListView: Boolean = false
+
+    lateinit private var staggeredLayoutManager: StaggeredGridLayoutManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        isListView = true
+
+        staggeredLayoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+        recyclerView.layoutManager = staggeredLayoutManager
+    }
+
+    private fun setUpActionBar() {
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        this.menu = menu
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.action_toggle) {
+            toggle()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun toggle() {
+        if (isListView) {
+            showGridView()
+        } else {
+            showListView()
+        }
+    }
+
+    private fun showListView() {
+        val item = menu.findItem(R.id.action_toggle)
+        item.setIcon(R.drawable.ic_action_grid)
+        item.title = getString(R.string.show_as_grid)
+        isListView = true
+    }
+
+    private fun showGridView() {
+        val item = menu.findItem(R.id.action_toggle)
+        item.setIcon(R.drawable.ic_action_list)
+        item.title = getString(R.string.show_as_list)
+        isListView = false
+    }
+}
